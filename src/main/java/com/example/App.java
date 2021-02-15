574e42a2352724adf61d51c836dcf231d1f861fe
Gerhard Ziesler <gerhard.ziesler@gmail.com>
2021-02-15 11:30:11 +0100

    refactor: Add ESignAnywhereClient

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
+import osplus.esignanywhere.v4.ESignAnywhereClient;
+import osplus.esignanywhere.v4.ESignAnywhereClientException;
 import osplus.esignanywhere.v4.api.EnvelopeApi;
 import osplus.esignanywhere.v4.api.SspFileApi;
 import osplus.esignanywhere.v4.api.VersionApi;
@@ -59,6 +61,18 @@ public class App implements Runnable {
         this.args = args;
     }
 
+    private ESignAnywhereClient esignAnywhereClient = null;
+
+    private ESignAnywhereClient getESignAnywhereClient() {
+        if (esignAnywhereClient == null) {
+            esignAnywhereClient = ESignAnywhereClient.builder()
+                .setBasePath("https://saas.esignanywhere.net/Api")
+                .addHeader("ApiToken", "mh24gps8u1xkqaxp4l34sgmm6qnxn438b1acq510fjk5vjqtdqilzgm7utgvrq7a")
+                .build();
+        }
+        return esignAnywhereClient;
+    }
+
     private ApiClient apiClient = null;
 
     private ApiClient getApiClient() {
@@ -108,6 +122,7 @@ public class App implements Runnable {
         return Files.readAllBytes(Paths.get(resource.toURI()));
     }
 
+    /*
     @Override
     public void run() {
         try {
@@ -259,6 +274,161 @@ public class App implements Runnable {
             }
         }  catch (final Exception e) {
             e.printStackTrace();
+        }
+    }
+    */
+
+    @Override
+    public void run() {
+        try {
+            final FlowApiResult version = getESignAnywhereClient().getVersion();
+            System.out.println("Version: " + version);
+
+            final UploadSspFileResult sspUploadFileResult1 = getESignAnywhereClient().uploadFile(getTestPdf());
+            System.out.println("UploadSspFileResult1: " + sspUploadFileResult1);
+
+            final UploadSspFileResult sspUploadFileResult2 = getESignAnywhereClient().uploadFile("Test2.pdf", getTestPdfAsByteArray());
+            System.out.println("UploadSspFileResult2: " + sspUploadFileResult2);
+
+            final UploadSspFileResult sspUploadFileResult3 = getESignAnywhereClient().uploadFile("Test3.pdf", getTestPdfAsByteArray());
+            System.out.println("UploadSspFileResult3: " + sspUploadFileResult3);
+
+            final SendEnvelopeRecipient sendEnvelopeRecipient1 = new SendEnvelopeRecipient();
+            sendEnvelopeRecipient1.email("gerhard.ziesler@gmail.com").firstName("Gerhard").lastName("Ziesler")
+                    .languageCode("de").emailBodyExtra("").disableEmail(false).addAndroidAppLink(false)
+                    .addIosAppLink(false).addWindowsAppLink(false).allowDelegation(true)
+                    .skipExternalDataValidation(false);
+
+            final SendEnvelopeRecipient sendEnvelopeRecipient2 = new SendEnvelopeRecipient();
+            sendEnvelopeRecipient2.email("gerhard.ziesler@gmail.com").firstName("Gerhard").lastName("Ziesler")
+                    .languageCode("de").emailBodyExtra("").disableEmail(false).addAndroidAppLink(false)
+                    .addIosAppLink(false).addWindowsAppLink(false).allowDelegation(false)
+                    .skipExternalDataValidation(false);
+
+            final ClientAction clientAction = new ClientAction();
+            clientAction.removeDocumentFromRecentDocumentList(false).callClientActionOnlyAfterSuccessfulSync(false)
+                    .clientName("SIGNificant SignAnywhere").closeApp(true).action("https://www.esignanywhere.net");
+
+            final FinishAction finishAction = new FinishAction();
+            finishAction.addClientActionsItem(clientAction);
+
+            final UserInformation senderUserInformation = new UserInformation();
+            senderUserInformation.firstName("Gerhard").lastName("Ziesler").email("gerhard.ziesler@gmail.com");
+
+            final UserInformation receiveUserInformation = new UserInformation();
+            receiveUserInformation.firstName("Gerhard").lastName("Ziesler").email("gerhard.ziesler@extern.f-i.de");
+
+            final ReceiverInformation receiverInformation = new ReceiverInformation();
+            receiverInformation.userInformation(receiveUserInformation);
+
+            final SenderInformation senderInformation = new SenderInformation();
+            senderInformation.userInformation(senderUserInformation);
+
+            final VisibleAreaOptions visibleAreaOptions = new VisibleAreaOptions();
+            visibleAreaOptions.allowedDomain("*").enabled(true);
+
+            final ViewerPreferences viewerPreferences = new ViewerPreferences();
+            viewerPreferences.finishWorkstepOnOpen(false).visibleAreaOptions(visibleAreaOptions);
+
+            final ResourceUris resourceUris = new ResourceUris();
+            resourceUris.delegationUri("https://demo.xyzmo.com/Resource/Delegate");
+
+            final AuditingToolsConfiguration auditingToolsConfiguration = new AuditingToolsConfiguration();
+            auditingToolsConfiguration.writeAuditTrail(false);
+
+            final GeneralPolicies generalPolicies = new GeneralPolicies();
+            generalPolicies.allowSaveDocument(true).allowSaveAuditTrail(true).allowRotatingPages(false)
+                    .allowEmailDocument(true).allowPrintDocument(true).allowFinishWorkstep(true)
+                    .allowRejectWorkstep(true).allowRejectWorkstepDelegation(true).allowUndoLastAction(true)
+                    .allowAdhocPdfAttachments(false).allowAdhocSignatures(false).allowAdhocStampings(false)
+                    .allowAdhocFreeHandAnnotations(false).allowAdhocTypewriterAnnotations(false)
+                    .allowAdhocPictureAnnotations(false).allowAdhocPdfPageAppending(false);
+
+            final StampImprintConfiguration stampImprintConfiguration = new StampImprintConfiguration();
+            stampImprintConfiguration.displayExtraInformation(true).displayEmail(true).displayIp(true).displayName(true)
+                    .displaySignatureDate(true).fontFamily("Times New Roman").fontSize(11.0);
+
+            final SigTypeClick2Sign allowedSignatureType = new SigTypeClick2Sign();
+            allowedSignatureType.stampImprintConfiguration(stampImprintConfiguration)
+                    .id("89a057d6-8e35-410f-84d3-e26cf93da175").discriminatorType("SigTypeClick2Sign").preferred(true);
+
+            final Signature signature = new Signature();
+            signature.positionPage(1).position(new Position().positionX(68.0).positionY(68.0))
+                    .size(new Size().height(79.0).width(190.0))
+                    .addAdditionalParametersItem(new KeyValuePair().key("enabled").value("1"))
+                    .addAdditionalParametersItem(new KeyValuePair().key("positioning").value("onPage"))
+                    .addAdditionalParametersItem(new KeyValuePair().key("reg").value("1"))
+                    .addAdditionalParametersItem(new KeyValuePair().key("fd").value(""))
+                    .addAdditionalParametersItem(new KeyValuePair().key("fd_dateformat").value("dd-MM-yyyy HH:mm:ss"))
+                    .addAdditionalParametersItem(new KeyValuePair().key("fd_timezone").value("datetimeutc"))
+                    .addAllowedSignatureTypesItem(allowedSignatureType).useTimestamp(false).isRequired(true)
+                    .id("1#XyzmoDuplicateIdSeperator#Signature_5430c3b6-cc0d-26b1-86ea-3ba909701349").displayName("")
+                    .docRefNumber(2).discriminatorType("Signature");
+
+            final WorkstepTasks workstepTasks = new WorkstepTasks();
+            workstepTasks.pictureAnnotationMinResolution(0).pictureAnnotationMaxResolution(0)
+                    .pictureAnnotationColorDepth(PictureAnnotationColorDepthEnum.COLOR16M)
+                    .sequenceMode(SequenceModeEnum.NOSEQUENCEENFORCED).positionUnits(PositionUnitsEnum.PDFUNITS)
+                    .referenceCorner(ReferenceCornerEnum.LOWER_LEFT).addTasksItem(signature);
+
+            final Policy policy = new Policy();
+            policy.generalPolicies(generalPolicies).workstepTasks(workstepTasks);
+
+            final WorkstepConfiguration workstepConfiguration = new WorkstepConfiguration();
+            workstepConfiguration.workstepLabel("Test").smallTextZoomFactorPercent(100).finishAction(finishAction)
+                    .receiverInformation(receiverInformation).senderInformation(senderInformation)
+                    .viewerPreferences(viewerPreferences).resourceUris(resourceUris)
+                    .auditingToolsConfiguration(auditingToolsConfiguration).policy(policy);
+
+            final SendEnvelopeDocumentOption documentOptionsItem = new SendEnvelopeDocumentOption();
+            documentOptionsItem.documentReference("1").isHidden(false);
+
+            final SendEnvelopeStep sendEnvelopeStep1 = new SendEnvelopeStep();
+            sendEnvelopeStep1.orderIndex(1).addRecipientsItem(sendEnvelopeRecipient1).emailBodyExtra("")
+                    .recipientType(RecipientTypeEnum.SIGNER).workstepConfiguration(workstepConfiguration)
+                    .addDocumentOptionsItem(documentOptionsItem).useDefaultAgreements(true);
+
+            final SendEnvelopeStep sendEnvelopeStep2 = new SendEnvelopeStep();
+            sendEnvelopeStep2.orderIndex(2).addRecipientsItem(sendEnvelopeRecipient2).emailBodyExtra("")
+                    .recipientType(RecipientTypeEnum.CC).useDefaultAgreements(false);
+
+            final SendEnvelopeDescription sendEnvelopeDescription = new SendEnvelopeDescription();
+            sendEnvelopeDescription.name("'Super wichtig'")
+                    .emailSubject("Bitte unterzeichnen Sie das beiliegende Dokument")
+                    .emailBody(
+                            "Herr #RecipientFirstName# #RecipientLastName#\n\n#PersonalMessage#\n\nBitte unterzeichnen Sie das beiliegende Dokument #EnvelopeName#\n\nDie Frist zur Unterzeichnung des Dokuments läuft am #ExpirationDate# ab")
+                    .displayedEmailSender("").enableReminders(true).firstReminderDayAmount(5)
+                    .recurrentReminderDayAmount(3).beforeExpirationDayAmount(3).daysUntilExpire(28).callbackUrl("")
+                    .statusUpdateCallbackUrl("").addStepsItem(sendEnvelopeStep1).addStepsItem(sendEnvelopeStep2)
+                    .attachSignedDocumentsToEnvelopeLog(false);
+
+            final EnvelopeSendModel envelopeSendModel = new EnvelopeSendModel();
+            envelopeSendModel.addSspFileIdsItem(sspUploadFileResult1.getSspFileId())
+                    .addSspFileIdsItem(sspUploadFileResult2.getSspFileId())
+                    .addSspFileIdsItem(sspUploadFileResult3.getSspFileId())
+                    .sendEnvelopeDescription(sendEnvelopeDescription);
+
+            final SendEnvelopeResult sendEnvelopeResult = getESignAnywhereClient().sendEnvelope(envelopeSendModel);
+            System.out.println("SendEnvelopeResult: " + sendEnvelopeResult);
+            
+            EnvelopeStatus envelopeStatus = null;
+
+            for (;;) {
+                envelopeStatus = getESignAnywhereClient().getEnvelopeStatus(sendEnvelopeResult.getEnvelopeId());
+                System.out.println("EnvelopeStatus: " + envelopeStatus.getStatus());
+                if (envelopeStatus.getStatus() == StatusEnum.COMPLETED) {
+                    break;
+                }
+                Thread.sleep(2000);
+            }
+
+            for (final FinishedDocument finishedDocument : envelopeStatus.getBulks().get(0).getFinishedDocuments()) {
+                System.out.println("FinishedDocument: " + finishedDocument);
+                final byte[] finishedDocumentContent = getESignAnywhereClient().downloadCompletedDocument(finishedDocument.getFlowDocumentId());
+                Files.write(Paths.get(finishedDocument.getFileName()), finishedDocumentContent, StandardOpenOption.CREATE);
+            }
+        }  catch (final Exception e) {
+            System.err.println(e.getMessage());
         }
     }
 
